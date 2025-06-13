d755bd3d0e73ae9065c79b1a67c64c99bf937b74
Sofia Skommar <sofiaskommar@gmail.com>
2025-06-13 13:32:21 +0200

    feat: add tab navigation and screens for Home, Search, Saved, and Profile

---
+++ app/movies/[id].tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>Movie Details</Text>
+    </View>
+  );
+};
+export default MovieDetails;
