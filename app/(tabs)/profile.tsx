d054522c6c5824bb6222714e7259c8dbfcabafd7
Sofia Skommar <sofiaskommar@gmail.com>
2025-06-14 13:14:17 +0200

    feat: enhance Profile and Saved components with layout and image integration

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,12 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { Image, Text, View } from "react-native";
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex flex-col flex-1 items-center justify-center gap-5">
+        <Image source={icons.person} className="size-10" tintColor={"#fff"} />
+        <Text className="text-gray-500">Profile</Text>
+      </View>
     </View>
   );
 };
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,8 +1,12 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { Image, Text, View } from "react-native";
 const Saved = () => {
   return (
-    <View>
-      <Text>Saved</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex flex-col flex-1 items-center justify-center gap-5">
+        <Image source={icons.save} className="size-10" tintColor={"#fff"} />
+        <Text className="text-gray-500">Saved</Text>
+      </View>
     </View>
   );
 };
